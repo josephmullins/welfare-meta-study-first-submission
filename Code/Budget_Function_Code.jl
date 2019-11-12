17c874d6000e0406dc253f223a75a2a5dc3d5cab
filbabalievsky1 <39629186+filbabalievsky1@users.noreply.github.com>
2019-11-11 19:47:48 -0600

    storing edits

--- Code/Budget_Function_Code.jl
+++ Code/Budget_Function_Code.jl
@@ -26,7 +26,9 @@ I observe the following rules:
 =#
 println("Checkpoint 1")
 
-budget1=zeros(3,3,3,17,17*4,2,2,2)
+Dev_Years=18
+
+budget1=zeros(3,3,3,Dev_Years,Dev_Years*4,2,2,2)
 
 site=["CTJF" "FTP" "MFIP-LR-F"]
 
@@ -38,7 +40,7 @@ for i in 1:3
     First_Year[i]=(minimum(Control.Year))
 end
 
-Earnings=zeros(3,72) # I add some extra years for the parents whose kids will age out
+Earnings=zeros(3,Dev_Years*4+1) # I add some extra years for the parents whose kids will age out
 
 for i in 1:3
     Control = Quarterly_Data[Quarterly_Data[:, :Site] .== site[i], :]
@@ -51,7 +53,7 @@ for i in 1:3
     for j in 1:observed
         Earnings[i,j]=Control[j,:Monthly_Earnings]
     end
-    for j in (observed+1):72
+    for j in (observed+1):length(Earnings[1,:])
         Earnings[i,j]=coef(lm2)[1]+j*coef(lm2)[2]
     end
 end
@@ -68,12 +70,12 @@ CTJF started 2 years after the others. Hence, I need to shift some years by 2.
 =#
 
 
-SNAP=zeros(3,3,17*4) # first index is the number of kids, second is site, third is quarter
+SNAP=zeros(3,3,Dev_Years*4) # first index is the number of kids, second is site, third is quarter
 for i in 1:3
     SNAPRulesC = SNAPRules[SNAPRules[:, :NumChild] .== i, :]
     for j in 1:3
         SNAPRulesC2 = SNAPRulesC[SNAPRulesC[:, :year] .>= First_Year[j], :]
-            for k in 1:17
+            for k in 1:Dev_Years
                 for z in 1:4
                 SNAP[i,j,(4*k+z-4)]=SNAPRulesC2.MA[k]
                 end
@@ -86,7 +88,7 @@ SNAPRules
 
 # need to consider parents whose kids aged out
 SnapRules_0=SNAPRules[SNAPRules[:, :NumChild] .== 0, :]
-SNAP0=zeros(3,72)
+SNAP0=zeros(3,Dev_Years*4+1)
 for i in 1:3
 
     SnapRules_02 = SnapRules_0[SnapRules_0[:, :year] .>= First_Year[i], :]
@@ -95,7 +97,7 @@ for i in 1:3
                     SNAP0[i,(4*k+z-4)]=SnapRules_02.MA[k]
                 end
             end
-
+            SNAP0[i,73]=SnapRules_02.MA[19] # one more year for parents with aged-out kids
 end
 SNAP0=SNAP0.*3
 
@@ -108,13 +110,13 @@ Poverty Cutoff
 
 =#
 
-Poverty=zeros(3,3,68)# first index is the number of kids, second is site, third is quarter for compatibility
+Poverty=zeros(3,3,Dev_Years*4)# first index is the number of kids, second is site, third is quarter for compatibility
 PovGuideline
 for j in 1:3
     #PovGuideline2=PovGuideline[:,(i+1)]
     PovGuideline2=PovGuideline[PovGuideline[:, :year] .>= First_Year[j], :] # but we do site first
     for i in 1:3
-        for k in 1:17
+        for k in 1:Dev_Years
             for z in 1:4
             Poverty[i,j,(4*k+z-4)]=PovGuideline2[k,(i+1)]./4 # quarterly!!
             end
@@ -130,6 +132,7 @@ Benefits Guideline
 =#
 println("Checkpoint 2")
 State=["Connecticut" "Florida" "Minnesota"]
+YearVec=[1996 1994 1994]
 
 BenStd
 BS2=BenStd[BenStd[:, :NumChild] .== 1, :]
@@ -139,10 +142,10 @@ BS3=melt(BS2,:state)
 sort!(BS3, :variable)
 BS3[!,:YearString]=string.(BS3.variable)
 BS3[!,:Year]=parse.(Int64, BS3.YearString)
-BS3=BS3[BS3[:, :Year] .>= 1994, :]
+BS3=BS3[BS3[:, :Year] .>= 1996, :]
 BS3.value[1]
 
-Benefit=zeros(3,3,68)
+Benefit=zeros(3,3,Dev_Years*4)
 for i in 1:3
     BS2=BenStd[BenStd[:, :NumChild] .== i, :]
     for j in 1:3
@@ -152,15 +155,29 @@ for i in 1:3
         sort!(BS3, :variable)
         BS3[!,:YearString]=string.(BS3.variable)
         BS3[!,:Year]=parse.(Int64, BS3.YearString)
-        BS3=BS3[BS3[:, :Year] .>= 1994, :] # fix this for CT later on!!!
-        for k in 1:17
-            for z in 1:4
-            Benefit[i,j,(4*k+z-4)]=BS3.value[k] # I assume this was quarterly?
+        if i!=1
+        BS3=BS3[BS3[:, :Year] .>= 1994, :]
+            for k in 1:(Dev_Years)
+                for z in 1:4
+                    Benefit[i,j,(4*k+z-4)]=BS3.value[k] # I assume this was quarterly?
+                end
             end
+        else # this is ugly but I need to feed in something else for CT
+            BS3=BS3[BS3[:, :Year] .>= 1996, :]
+                for k in 1:(Dev_Years-2) # snip off the last 2 years
+                    for z in 1:4
+                        Benefit[i,j,(4*k+z-4)]=BS3.value[k] # I assume this was quarterly?
+                    end
+                end
+                for k in (Dev_Years-2):Dev_Years
+                    for z in 1:4
+                        Benefit[i,j,(4*k+z-4)]=BS3.value[Dev_Years-2] # I assume this was quarterly?
+                    end
+                end
         end
     end
 end
-
+Benefit
 
 
 # index 1 indicates false, 2 true
@@ -172,12 +189,12 @@ Program=[0,1]
 
 
 # budget(site,arm,NumChild,age0,quarter,eligible,program,work) (NS x NT x 3 x 17 x Q x 2 x 2 x 2)
-budget1=zeros(3,3,3,17,17*4,2,2,2)
-3*3*3*17*17*4*2*2*2
+budget1=zeros(3,3,3,Dev_Years,Dev_Years*4,2,2,2)
+3*3*3*Dev_Years*Dev_Years*4*2*2*2
 1+1
 println("Checkpoint 3")
-@time @inbounds @simd for a0 in 1:17 # outermost loop is for child initial age, 0-17 (shifted by 1-indexing)
-    @inbounds @simd    for q in 1:68 # next loop is for quarter
+@time @inbounds @simd for a0 in 1:Dev_Years # outermost loop is for child initial age, 0-17 (shifted by 1-indexing)
+    @inbounds @simd    for q in 1:(Dev_Years*4) # next loop is for quarter
     @inbounds @simd         for nk in 1:3 # num kids
     @inbounds @simd             for e in 1:2 # eligible or not
     @inbounds @simd                 for w in 1:2 # working or not
@@ -205,7 +222,7 @@ println("Checkpoint 3")
                                 elseif site==2
                                 Foodstamps_F=max(SNAP[nk,site, q]-0.3*max(0.8*Earnings[site,q]*Work[w]-134,0),0 )
                                 FTP=max(Benefit[nk,site,q]-0.5*max(Earnings[site,q]*Work[w]-200,0),0)
-                                budget1[site,2,nk,a0,q,e,p,w]=Program[p]*(FTP*Eligible[e]+(1-Eligible[e])*Foodstamps_F)+
+                                budget1[site,2,nk,a0,q,e,p,w]=Program[p]*(FTP*Eligible[e]+Foodstamps_F)+
                                                                             Earnings[site,q]*Work[w]
                                 budget1[site,3,nk,a0,q,e,p,w]=budget1[site,2,nk,a0,q,e,p,w]
                                 elseif site==3
@@ -225,14 +242,14 @@ println("Checkpoint 3")
         end
     end
 end
-budget1=budget1.+0.0000001
+
 minimum(budget1)
 findmax(budget1[:,3,:,:,:,:,:,:])
 
 
-Budget_Ageout=zeros(3,72,2,2)
+Budget_Ageout=zeros(3,Dev_Years*4+1,2,2)
 @time @inbounds @simd for site in 1:3
-    @inbounds @simd     for q in 1:72
+    @inbounds @simd     for q in 1:(Dev_Years*4+1)
     @inbounds @simd         for w in 1:2
     @inbounds @simd             for p in 1:2
                         Budget_Ageout[site,q,p,w]=Earnings[site,q]*Work[w]+Program[p]*max(SNAP0[site,q]-0.3*max(0.8*Earnings[site,q]*Work[w]-134,0),0)
@@ -240,8 +257,10 @@ Budget_Ageout=zeros(3,72,2,2)
         end
     end
 end
-Budget_Ageout
-Budget_Ageout=Budget_Ageout.+0.000001
+Budget_Ageout[:,73,2,2]
+
 TimeLimit_Ind=[false true true; false true true; false false false]
 
 TimeLimits=[0 7 7; 0 8 8; 0 0 0]
+
+Work_Reqs_Ind=[false true true; false true true; false false true]
