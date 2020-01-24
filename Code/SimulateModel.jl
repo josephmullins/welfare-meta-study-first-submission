2c07bd763edfa247f08e08e9aac8f4e0a0be97eb
filbabalievsky1 <39629186+filbabalievsky1@users.noreply.github.com>
2020-01-24 17:39:40 -0600

    Simulation running--weird thing with optimized pars
    The simulated and differentiable moment generating functions yield the same moment distance for the baseline parameter guess, but diverge as I optimize the differentiable code

--- Code/SimulateModel.jl
+++ Code/SimulateModel.jl
@@ -1,6 +1,6 @@
 using Revise
 using Distributions
-
+cd("/Users/FilipB/github/welfare-meta-study/Code")
 includet("BaselineModel.jl")
 includet("EstimationBaseline.jl")
 includet("SetupBaseline.jl")
@@ -9,30 +9,15 @@ includet("SetupBaseline.jl")
 pars = parameters()
 vlist = [:αc, :αH, :αA, :σH, :σC, :αWR,:αWR2, :αF, :β]
 
-
-
-sample_size=[4803, 1405+1410,15683,4433,4554,8322]
+sample_size=[4803, 1405+1410,15683,3208,6009,4433,4554,8322]
 
  # old was [4803,1405+1410,3208,6009]
 
  # I got the numbers for NEWWS from line 139, and for LAGAIN from 374
 
-# Below: scratch notes to help me get under the hood of Jo's code
 
-sname=site_list[1]
-Y = getfield(budget,sname)
-
-moms=GetMomentsAll(pars,site_list,budget,moments,wghts,site_features)
-moms.CTJF
-
-
-site_features.yb
-
-site_list
-
-moms_collect = []
+# This block of code sets up some things I use to test the functions below
 αHT = [0;pars.αHT]
-π0
 yb = site_features.yb[1]
 T = site_features.T[1]
 years = (yb+1-1991):(yb-1991+T)
@@ -43,8 +28,6 @@ sname = site_list[1]
 Y = getfield(budget,sname)
 moms = getfield(moments,sname)
 π01 = site_features.π0[1,:,:]
-sum(π01)
-sum(π01[3,:])
 year_meas = site_features.year_meas[1]
 Abar = size(Y)[1]
 moms_model = zeros(size(moms))
@@ -54,55 +37,11 @@ abar = min(Abar,2)
 Y_I = budget[Symbol(sname,"_I")]
 TLlength = site_features.TLlength[1,2]
 NK = size(π01)[1]
-
 pA1,pWork1,pF1 = GetStaticProbs(pars_site,Y[abar,:,:,:,:],price,WR,T,NK)
 pA2,pWork2,pF2 = GetDynamicProbs(pars_site,Y[abar,:,:,:,:],Y_I[abar,:,:,:,:],price,WR,T,NK,TLlength)
+# end test code prep
 
-
-arm_selector=rand(Uniform())
-
-NK = size(π0)[1]
-pA,pWork,pF = GetDynamicProbs(pars,Y,Y_I,price,WR,T,NK,TLlength)
-
-# this is taken from the criterion function
-Qn = 0
-αHT = [0;pars.αHT]
-for i=1:length(site_list)
-    yb = site_features.yb[i]
-    T = site_features.T[i]
-    years = (yb+1-1991):(yb-1991+T)
-    pos = sum(site_features.T[1:i-1])
-    #αH = pars.αH[(pos+1):(pos+site_features.T[i])]
-    pars_site = GetSitePars(pars,i)
-    sname = site_list[i]
-    Y = getfield(budget,sname)
-    moms = getfield(moments,sname)
-    π1 = site_features.π0[i,:,:]
-    year_meas = site_features.year_meas[i]
-    Abar = size(Y)[1]
-    moms_model = zeros(size(moms))
-    for a = 1:site_features.n_arms[i]
-        WR = site_features.work_reqs[i,a]
-        price = site_features.prices[i,a]
-        abar = min(Abar,a)
-        if site_features.time_limits[i,a]==1
-            Y_I = budget[Symbol(sname,"_I")]
-            TLlength = site_features.TLlength[i,a]
-            moms_model[:,a] = GetMomentsTimeLims(pars_site,Y[abar,:,:,:,:],Y_I[abar,:,:,:,:],price,WR,T,π1,TLlength,year_meas)
-        else
-            moms_model[:,a] = GetMoments(pars_site,Y[abar,:,:,:,:],price,WR,T,π1,year_meas)
-        end
-    end
-    append!(moms_collect,[moms_model])
-end
-return q1=(;zip(site_list,moms_collect)...)
-q1
-
-moms_collect
-#simulation pseudocode
-
-
-function draw_kids(π0, site)
+function draw_kids(π0, site,site_features)
     # get NK
     π01 = site_features.π0[site,:,:]
     prob_nk=zeros(3)
@@ -115,10 +54,14 @@ function draw_kids(π0, site)
     nk=0
 
     A1=zeros(3)
-    for i in 1:3
+    for i in 2:3
         A1[i]=ifelse(nk_draw>=prob_nk[i],1.0,0)
     end
     nk=convert(Int,sum(A1))+1
+    if nk>3
+        println(nk)
+        println(A1)
+    end
 
     # get age
     π02 = site_features.π0[site,nk,:]
@@ -139,7 +82,8 @@ function draw_kids(π0, site)
 
     return nk, age
 end
-draw_kids(π0,1)
+draw_kids(π0,1,site_features)
+
 
 
 function decisions_static(t,nk, age,pA,pWork,pF)
@@ -192,13 +136,10 @@ end
 decisions_dynamic(1,2,3,1,pA2,pWork2,pF2)
 
 
-# this is for each arm
 
 
 
-
-
-function Moments_Simulated_Static(nsims, pA,pF,pWork, π1, sample_size_site,T, site)
+function Moments_Simulated_Static(nsims, pA,pF,pWork, π1, sample_size_site,T, site,site_features)
 
     A_mat=zeros(T,nsims, sample_size_site)
     A_sims=zeros(T,nsims)
@@ -215,7 +156,7 @@ function Moments_Simulated_Static(nsims, pA,pF,pWork, π1, sample_size_site,T, s
     # note: only give benefits if kids aged 0-9, aka indices 1-10
     for n in 1:nsims
         for i in 1:sample_size_site
-            nk, age=draw_kids(π1,site)
+            nk, age=draw_kids(π1,site,site_features)
             a=age
             for t in 1:T
                 A_mat[t,n,i],W_mat[t,n,i],F0=decisions_static(t,nk, age,pA,pWork,pF)
@@ -240,12 +181,11 @@ function Moments_Simulated_Static(nsims, pA,pF,pWork, π1, sample_size_site,T, s
 
     return A,W,F
 end
+@time A,W,F=Moments_Simulated_Static(2,pA1,pF1,pWork1, π01, sample_size[1],4,1,site_features)
+@time Moments_Simulated_Static(2,pA1,pF1,pWork1, π01, sample_size[1],4,1,site_features)
 
-@time Moments_Simulated_Static(2,pA1,pF1,pWork1, π01, sample_size[1],4,1)
-@time Moments_Simulated_Static(2,pA1,pF1,pWork1, π01, sample_size[1],4,1)
-1+1
 
-function Moments_Simulated_Dynamic(nsims, pA,pF,pWork, π1, sample_size_site,T, site, tlsite)
+function Moments_Simulated_Dynamic(nsims, pA,pF,pWork, π1, sample_size_site,T, site, tlsite,site_features)
     A_mat=zeros(T,nsims, sample_size_site)
     A_sims=zeros(T,nsims)
     A=zeros(T)
@@ -264,7 +204,7 @@ function Moments_Simulated_Dynamic(nsims, pA,pF,pWork, π1, sample_size_site,T, 
     for n in 1:nsims
         for i in 1:sample_size_site
             tl=1
-            nk, age=draw_kids(π1,site)
+            nk, age=draw_kids(π1,site,site_features)
             a=age
             for t in 1:T
                 A_mat[t,n,i],W_mat[t,n,i],F0=decisions_dynamic(t,nk,age,tl,pA,pWork, pF)
@@ -293,40 +233,87 @@ function Moments_Simulated_Dynamic(nsims, pA,pF,pWork, π1, sample_size_site,T, 
     return A,W,F
 
 end
-
-
-@time Moments_Simulated_Dynamic(2,pA2,pF2,pWork2, π01, sample_size[1],4,1,3)
-@time Moments_Simulated_Dynamic(2,pA2,pF2,pWork2, π01, sample_size[1],4,1,3)
-
-convert(Int,(round(1.5)))
+@time A,W,F=Moments_Simulated_Dynamic(2,pA2,pF2,pWork2, π01, sample_size[1],4,1,3,site_features)
+mom_d=[A;W;F]
+@time Moments_Simulated_Dynamic(2,pA2,pF2,pWork2, π01, sample_size[1],4,1,3,site_features)
 
 
 
+function Get_Simulated_MomentsAll(pars,site_list,budget,moments,wghts,site_features,nsims,sample_size)
+    moms_collect = []
+    Qn = 0
+    αHT = [0;pars.αHT]
+    for i=1:length(site_list)
+        NK=3
+        yb = site_features.yb[i]
+        T = site_features.T[i]
+        years = (yb+1-1991):(yb-1991+T)
+        pos = sum(site_features.T[1:i-1])
+        #αH = pars.αH[(pos+1):(pos+site_features.T[i])]
+        pars_site = GetSitePars(pars,i)
+        sname = site_list[i]
+        Y = getfield(budget,sname)
+        moms = getfield(moments,sname)
+        π1 = site_features.π0[i,:,:]
+        wght = getfield(wghts,sname)
+        year_meas = site_features.year_meas[i]
+        Abar = size(Y)[1]
+        moms_model = zeros(size(moms))
+        price = site_features.prices[i,1]
+        pA1,pWork1,pF1 = GetStaticProbs(pars_site,Y[1,:,:,:,:],price,0,T,NK) # last 3 are WR, T, NK
+        ssize2=convert(Int,(round(sample_size[i]))) # will replace later with actual arm sample sizes
+        A,W,F=Moments_Simulated_Static(nsims, pA1,pF1,pWork1, π1, ssize2,T, i,site_features)
+        moms_control = [A;W;F]
+        Qn += sum(wght[:,1].*(moms[:,1] .- moms_control).^2)
+        moms_model[:,1]=moms_control
 
 
-for k in 1:length(nsims)
-    #arm_selector=rand(Uniform())
-    #arm=0 # treatment or control
-    #if arm_selector>=0.5
-    #    arm=1
-    #end
-
-    # draw random to select number of kids
-    # for everyone in sample size
-
-            for i in 1:length(sample_size[1]) # for everyone in sample size
-            nk,age=draw_kids(π0,1)
-
-        for t in 1:T
-            # draw uniform random to decide if work or not
-            # draw uniform random to decide if participate or not
-                # update time limit if participate
-                # draw uniform random  to decide if formal or not
-            # It seems the age index is fixed and only the time index changes?
+        for a = 2:site_features.n_arms[i]
+            WR = site_features.work_reqs[i,a]
+            price = site_features.prices[i,a]
+            abar = min(Abar,a)
+            ssize2=convert(Int,(round(sample_size[i])))
+            if site_features.time_limits[i,a]==1
+                Y_I = budget[Symbol(sname,"_I")]
+                TLlength = site_features.TLlength[i,a]
+                pA2,pWork2,pF2 = GetDynamicProbs(pars_site,Y[abar,:,:,:,:],Y_I[abar,:,:,:,:],price,WR,T,NK,TLlength)
+                A,W,F=Moments_Simulated_Dynamic(2,pA2,pF2,pWork2, π01, ssize2,T,i,TLlength,site_features)#(nsims, pA,pF,pWork, π1, sample_size_site,T, site, tlsite,site_features)
+                moms_treatment = [A;W;F]
+                moms_model[:,a]=moms_treatment
+            else
+                pA1,pWork1,pF1 = GetStaticProbs(pars_site,Y[abar,:,:,:,:],price,WR,T,NK) # last 3 are WR, T, NK
+                A,W,F=Moments_Simulated_Static(nsims, pA1,pF1,pWork1, π1, ssize2,T, i,site_features)
+                moms_treatment = [A;W;F]
+                moms_model[:,a]=moms_treatment
             end
+            TE = moms[:,a] .- moms[:,1]
+            TE_mod = moms_treatment .- moms_control
+            Qn += sum(wght[:,a].*(TE .- TE_mod).^2)
         end
-
+        append!(moms_collect,[moms_model])
+    end
+    return Qn, (;zip(site_list,moms_collect)...)
 end
 
-test=randn(2,2)
-test2=zeros(2)
+# check with baseline params
+pars = parameters()
+Get_Simulated_MomentsAll(pars,site_list,budget,moments,wghts,site_features,10,sample_size)
+CriterionP(pars,site_list,budget,moments,wghts,site_features)
+moments
+GetMomentsAll(pars,site_list,budget,moments,wghts,site_features)
+
+
+# check with optimized params
+vlist0 = [:αc, :αH, :αA, :αF]
+opt,x0 = GetOptimization(pars,vlist0,site_list,budget,moments,wghts,site_features)
+res = optimize(opt,x0)
+pars = UpdatePars(res[2],pars,vlist0)
+Get_Simulated_MomentsAll(pars,site_list,budget,moments,wghts,site_features,10,sample_size)
+CriterionP(pars,site_list,budget,moments,wghts,site_features)
+
+# with further optimization the gap in the criterion functions grows
+opt,x0 = GetOptimization(pars,vlist,site_list,budget,moments,wghts,site_features)
+res2 = optimize(opt,x0)
+pars2 = UpdatePars(res2[2],pars,vlist)
+Get_Simulated_MomentsAll(pars2,site_list,budget,moments,wghts,site_features,10,sample_size)
+CriterionP(pars2,site_list,budget,moments,wghts,site_features)
